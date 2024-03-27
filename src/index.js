9541539038ff0b9250df464cb7f62877e03f97e9
Chloe Yeo <yeochloe01@gmail.com>
2024-03-27 21:10:38 +0900

    second swiper implemented but first swiper broke

--- src/index.js
+++ src/index.js
@@ -3,8 +3,39 @@ const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 5000,
   },
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const menuSwiper = new Swiper(".swiper", {
+  slidesPerView: 3,
+  spaceBetween: 0,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  // Pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 const swiperSlide = document.querySelector(".swiper").swiper;
+const menuSwiperSlide = document.querySelector(".menuSwiper").menuSwiper;
 
 swiperSlide.slideNext();
