be9f02e93651a4b6573efb7b44865de8336b16de
kiwoom <dk960227@hanmail.net>
2024-03-31 10:28:10 +0900

    0331modalWithMap_BOSCO

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 // 1) Header_start
 
 // 1) Header_end
@@ -52,7 +53,7 @@ const menuSwiper = new Swiper(".menu-swiper", {
 });
 
 const swiperSlide = document.querySelector(".swipWrap").swiper;
-const menuSwiperSlide = document.querySelector(".menuSwiper").menuSwiper;
+// const menuSwiperSlide = document.querySelector(".menuSwiper").menuSwiper;
 
 swiperSlide.slideNext();
 
@@ -67,7 +68,38 @@ swiperSlide.slideNext();
 // 6) contentsWrap_end
 
 // 7) contentsBox_start
- 
+
+const modal = document.getElementById("myModal");
+
+// 모달을 닫는 <span> 요소를 가져옵니다
+const span = document.getElementsByClassName("close")[0];
+
+// 검색 버튼에 이벤트 리스너 추가
+document.getElementById("searchBtn").onclick = function() {
+const searchValue = document.getElementById("searchInput").value;
+document.getElementById("searchTerm").innerText = searchValue;
+modal.style.display = "block";
+}
+
+// <span> (x)를 클릭하면 모달을 닫습니다
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// 모달 외부를 클릭하면 모달을 닫습니다
+window.onclick = function(event) {
+if (event.target == modal) {
+  modal.style.display = "none";
+}
+}
+const container = document.getElementById('map');
+
+const options = {
+	center: new kakao.maps.LatLng(37.539716, 126.851020), //지도의 중심좌표.
+	level: 4
+};
+
+const map = new kakao.maps.Map(container, options);
 // 7) contentsBox_end
 
 // 8) botBoxWrap_start
